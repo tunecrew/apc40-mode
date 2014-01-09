52172cac1c39a4558a3afc9a1522f78378118fe3
Semyon Proshev <s-proshev@ya.ru>
2014-01-09 17:55:14 +0400

    gui

--- windows-linux/APC40Mode.java
+++ windows-linux/APC40Mode.java
@@ -1,29 +1,14 @@
 enum APC40Mode {
     Generic, AbletonLive, AlternateAbletonLive;
 
-    public static APC40Mode getMode(int modeNumber) {
-        switch (modeNumber) {
-            case 0: return Generic;
-            case 1: return AbletonLive;
-            case 2: return AlternateAbletonLive;
-        }
-        throw new IllegalArgumentException("Unsupported mode number: " + modeNumber);
-    }
-
-    public int getNumber() {
-        switch (this) {
-            case Generic: return 0;
-            case AbletonLive: return 1;
-            case AlternateAbletonLive: return 2;
-        }
-        throw new RuntimeException("Unsupported enum: " + this);
-    }
-
     public byte getByte() {
         switch (this) {
-            case Generic: return (byte) 0x40;
-            case AbletonLive: return (byte) 0x41;
-            case AlternateAbletonLive: return (byte) 0x42;
+            case Generic:
+                return (byte) 0x40;
+            case AbletonLive:
+                return (byte) 0x41;
+            case AlternateAbletonLive:
+                return (byte) 0x42;
         }
         throw new RuntimeException("Unsupported enum: " + this);
     }
@@ -31,9 +16,12 @@ enum APC40Mode {
     @Override
     public String toString() {
         switch (this) {
-            case Generic: return "Generic Mode";
-            case AbletonLive: return "Ableton Live Mode";
-            case AlternateAbletonLive: return "Alternate Ableton Live Mode";
+            case Generic:
+                return "Generic Mode";
+            case AbletonLive:
+                return "Ableton Live Mode";
+            case AlternateAbletonLive:
+                return "Alternate Ableton Live Mode";
         }
         throw new RuntimeException("Unsupported enum: " + this);
     }
--- windows-linux/Main.java
+++ windows-linux/Main.java
@@ -1,38 +1,55 @@
 import com.sun.media.sound.MidiOutDeviceProvider;
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 
 import javax.sound.midi.*;
 
-public class Main {
+public class Main extends Application {
     private static final String APC40_NAME = "APC40";
 
     public static void main(String[] args)
             throws MidiUnavailableException, InvalidMidiDataException {
-        if (args.length == 1) {
-            try {
-                APC40Mode mode = APC40Mode.getMode(Integer.parseInt(args[0]));
-                System.out.println("Selected mode is \"" + mode + "\"");
-                applyMode(mode);
-            } catch (IllegalArgumentException e) {
-                System.out.println(getUsageMessage());
-            }
-        } else {
-            System.out.println(getUsageMessage());
-        }
+        launch(args);
     }
 
-    private static void applyMode(APC40Mode mode)
+    @Override
+    public void start(Stage stage) throws Exception {
+        HBox hbox = new HBox();
+        hbox.setPadding(new Insets(15, 12, 15, 12));
+        hbox.setSpacing(10);
+        for (APC40Mode mode : APC40Mode.values()) {
+            Button button = new Button(mode.toString());
+            button.setOnAction(new ModeButtonClickHandler(mode));
+            hbox.getChildren().add(button);
+        }
+        Scene scene = new Scene(hbox);
+        stage.setScene(scene);
+        stage.show();
+        stage.setOnCloseRequest(new CloseRequestHandler());
+    }
+
+    private synchronized void applyMode(APC40Mode mode)
             throws MidiUnavailableException, InvalidMidiDataException {
         MidiDevice midiOutputDevice = getMidiOutputDevice();
         if (midiOutputDevice != null) {
             midiOutputDevice.open();
             Receiver midiOutputReceiver = midiOutputDevice.getReceiver();
-            byte[] message = {(byte)0xf0, 0x47, 0x00, 0x73, 0x60, 0x00, 0x04, mode.getByte(), 0x08, 0x04, 0x01, (byte)0xf7};
+            byte[] message = {(byte) 0xf0, 0x47, 0x00, 0x73, 0x60, 0x00, 0x04, mode.getByte(), 0x08, 0x04, 0x01, (byte) 0xf7};
             SysexMessage sysexMessage = new SysexMessage(message, message.length);
             midiOutputReceiver.send(sysexMessage, -1);
             midiOutputReceiver.close();
             midiOutputDevice.close();
+            printMessage("\"" + mode.toString() + "\" is turned on");
         } else {
-            System.out.println(APC40_NAME + " not found");
+            printMessage(APC40_NAME + " not found");
         }
     }
 
@@ -48,11 +65,32 @@ public class Main {
         return null;
     }
 
-    private static String getUsageMessage() {
-        StringBuilder sb = new StringBuilder("Usage:\n");
-        for (APC40Mode mode : APC40Mode.values()) {
-            sb.append("\t").append(mode.getNumber()).append(" - ").append(mode).append("\n");
+    private void printMessage(String message) {
+        System.out.println(message);
+    }
+
+    private class ModeButtonClickHandler implements EventHandler<ActionEvent> {
+        private final APC40Mode mode;
+
+        public ModeButtonClickHandler(APC40Mode mode) {
+            this.mode = mode;
         }
-        return sb.toString();
+
+        @Override
+        public void handle(ActionEvent actionEvent) {
+            try {
+                applyMode(mode);
+            } catch (MidiUnavailableException | InvalidMidiDataException e) {
+                printMessage(e.getMessage());
+            }
+        }
+    }
+
+    private class CloseRequestHandler implements EventHandler<WindowEvent> {
+        @Override
+        public void handle(WindowEvent windowEvent) {
+            Platform.exit();
+            System.exit(0);
+        }
     }
 }
